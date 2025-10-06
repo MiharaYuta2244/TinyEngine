9518685f3618d3520ffcc8dc6752429f0e70e030
Ratta38 <k024g1030@g.neec.ac.jp>
2025-10-07 08:59:07 +0900

    test

--- project/main.cpp
+++ project/main.cpp
@@ -2,6 +2,14 @@
 #include "WinApp.h"
 #include <memory>
 
+#if defined(_MSC_VER) && !defined(_rotr64)
+#include <intrin.h>
+inline unsigned __int64 _rotr64(unsigned __int64 value, unsigned int shift) noexcept {
+	shift &= 63;
+	return (value >> shift) | (value << (64 - shift));
+}
+#endif
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int) {
 	// ゲームクラス生成
 	std::unique_ptr<Game> game = std::make_unique<Game>();
